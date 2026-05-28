3b401dbcadbd27fbbca64ec28d923559cd67caa0
Nikhil Rao <nrao200423@gmail.com>
2026-05-27 20:43:08 -0700

    added normalize features helper

--- main.py
+++ main.py
@@ -21,3 +21,46 @@ def load_data(file_name):
             dataset_rows.append((class_label, feature_values))
 
     return dataset_rows
+
+def normalize_features(dataset_rows):
+    # min-max normalize each feature column:
+    # normalized = (value - min) / (max - min)
+    if len(dataset_rows) == 0:
+        return dataset_rows
+
+    total_features = len(dataset_rows[0][1])
+    if total_features == 0:
+        return dataset_rows
+
+    # initialize min and max trackers
+    column_mins = [float("inf")] * total_features
+    column_maxs = [float("-inf")] * total_features
+
+    # scan all rows once to get each feature's min and max
+    for class_label, feature_values in dataset_rows:
+        for feature_index in range(total_features):
+            current_value = feature_values[feature_index]
+            if current_value < column_mins[feature_index]:
+                column_mins[feature_index] = current_value
+            if current_value > column_maxs[feature_index]:
+                column_maxs[feature_index] = current_value
+
+    # build normalized dataset
+    normalized_rows = []
+    for class_label, feature_values in dataset_rows:
+        normalized_values = []
+        for feature_index in range(total_features):
+            min_value = column_mins[feature_index]
+            max_value = column_maxs[feature_index]
+            denominator = max_value - min_value
+
+            if denominator == 0:
+                # if all values in this column are the same, set to 0
+                normalized_values.append(0.0)
+            else:
+                normalized_number = (feature_values[feature_index] - min_value) / denominator
+                normalized_values.append(normalized_number)
+
+        normalized_rows.append((class_label, normalized_values))
+
+    return normalized_rows
